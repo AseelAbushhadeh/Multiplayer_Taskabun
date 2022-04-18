d5fafbbc69a2b197fe19f04eac76dc0209074fbf
AseelAbushhadeh <aseelsam99@gmail.com>
2022-04-18 21:53:06 +0300

    camera2d

--- Game/Camera2D.gd
+++ Game/Camera2D.gd
@@ -12,17 +12,5 @@ func _process(delta: float) -> void:
 			else:
 				global_position = lerp(global_position, target_player.global_position, delta * 10)
 
-var x=1
-var y=1
-func _on__pressed():
-	if x<2:
-		x+=.5
-		y+=.5
-		zoom=Vector2(x,y)
 
 
-func _on_plus_pressed():
-	if x>1:
-		x-=.5
-		y-=.5
-		zoom=Vector2(x,y)
